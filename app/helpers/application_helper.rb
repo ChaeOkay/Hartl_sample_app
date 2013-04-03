6ebd275655c48799e5d1aece81995f4ef2a5bafa
cokeefe <chaemobile@gmail.com>
2013-04-03 15:00:08 -0400

    chpt 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  #Returns the full title on a per-page basis.
+  #helpesr are automatically included in views with rails
+  def full_title(page_title)                          #method
+    base_title = "Ruby on Rails Tutorial Sample App"  #variable
+    if page_title.empty?                              #boolean
+      base_title                                      #Implicit return
+    else
+      "#{base_title} | #{page_title}"                 #string
+    end
+  end
+  
 end
