eeca2491f39133dd2090d19faa1a35da6609d0c5
cokeefe <chaemobile@gmail.com>
2013-04-07 18:46:30 -0400

    7.2.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,10 +35,5 @@ SampleApp::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
   
-  # Speed up test by lowering BCyrypt's cost function.
-  require 'bcrypt' 
-  silence_warnings do
-    BCrypt::Engine::DEFAULT_COST = Bcrypt::Engine::MIN_COST
-    end
-  end
+    
 end
