63379f5f1aa90adef76e936b60a7ef57c90312b8
cokeefe <chaemobile@gmail.com>
2013-04-08 10:17:44 -0400

    to 7.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_path
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  
 end
