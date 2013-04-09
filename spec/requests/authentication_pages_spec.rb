e9047fa9710a7ce6539ffd6d611ed58721d04117
cokeefe <chaemobile@gmail.com>
2013-04-09 13:06:45 -0400

    Finish 8

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,11 +19,13 @@ describe "AuthenticationPages" do
         before { click_button "Sign in" }
         
         it { should have_selector('title', text: 'Sign in') }
-        it { should have_selector('div.alert.alert-error', text: 'Invalid') } 
+      #  it { should have_selector('div.alert.alert-error', text: 'Invalid') } 
+        it { should have_error_message } #with Matchers - see Utilities
       
         describe "after visiting another page" do
           before { click_link "Home" }
-          it { should_not have_selector('div.alert.alert-error') }
+       #   it { should_not have_selector('div.alert.alert-error') }
+         it { should_not have_error_message } #with Matchers - see Utilities
         end
         
         describe "with valid information" do
